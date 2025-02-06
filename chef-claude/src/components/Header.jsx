60b544e10af421164ae03f2da6b25c34952f09e3
simaaksidd <simaaksidd1@gmail.com>
2025-02-06 15:32:07 -0600

    chef claude static components done

---
+++ chef-claude/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import image from '../assets/chef-claude-icon.png'
+
+export default function Header() {
+    return (
+        <header>
+            <img src={image} alt="chef claude icon"></img>
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
