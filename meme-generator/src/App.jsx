6ff68252df90092a153b278f0c3b28a738b5f9f7
simaaksidd <simaaksidd1@gmail.com>
2025-02-07 14:01:47 -0600

    add meme generator static components

---
+++ meme-generator/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header.jsx"
+import MainContent from "./components/MainContent.jsx"
+import './App.css'
+
+export default function App() {
+    
+    return (
+        <>
+            <Header />
+            <MainContent />
+        </>
+    )
+}
